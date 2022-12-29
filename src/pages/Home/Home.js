8cabd8e6f4556ba665d6fbb893a8bf78882784fa
Rawitch Payakkawan <rawitch13@gmail.com>
2022-12-29 15:05:48 +0700

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      Home
+    </>
+  );
+}
+
+export default Home;
